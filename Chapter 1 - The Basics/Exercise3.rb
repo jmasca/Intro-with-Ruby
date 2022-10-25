74a0abf2b1ac2d78bc833aede0d8e0f32d6de40b
Jeffery Masca <j.masca922@gmail.com>
2022-10-25 15:42:20 -0400

    ch1 ex3,4 - completed

---
+++ Chapter 1 - The Basics/Exercise3.rb
@@ -0,0 +1,12 @@
+#Using a hash to store movie titles and their release year.
+movies = {
+	:memento => 2000,
+	:the_truman_show => 1998,
+	:monsters_inc => 2001,
+	:hocus_pocus => 1993,
+	:bridesmaids => 2011,
+}
+
+puts movies[:memento], movies[:the_truman_show], movies[:monsters_inc],
+	movies[:hocus_pocus], movies[:bridesmaids]
+	
---
+++ Chapter 1 - The Basics/Exercise4.rb
@@ -0,0 +1,4 @@
+#Storing movie dates from Exercise3 in an array.
+dates = [2000, 1998, 2001, 1993, 2011]
+
+puts dates[0], dates[1], dates[2], dates[3], dates[4]
